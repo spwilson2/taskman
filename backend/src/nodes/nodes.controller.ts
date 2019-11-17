892652d5f7624bf8bec03a41c3de649bc88a8ac1
Sean Wilson <sean.wilson@ghs.com>
2019-11-16 19:55:50 -0800

    /nodes: Add POST support to add node(s)
    This commit adds support for sending POST commands to the /nodes
    endpoint. POSTs with invalid formatting of data will receive
    a BAD_REQUEST response along with a stringified message of the exception
    which caused the error. This could be a security hole at some point and
    should probably be addressed, but for now we hope no unexpected
    exceptions will arise with sensitive data.
    
    This commit has reduced code quality by placing all class definitions in
    the nodes.controller.ts file. This was done to improve development speed
    but should be undone in a future commit.

--- backend/src/nodes/nodes.controller.ts
+++ backend/src/nodes/nodes.controller.ts
@@ -1,15 +1,139 @@
 import * as express from 'express';
 import * as httpStatus from 'http-status-codes';
-import Node from './node.interface';
+//import Node from './node.interface';
+//
 
-interface NodeRequestMetadata {
-    type: string;
-    parent: string;
+class TemplateContainsId extends Error {}
+class TemplateMissingMetadata extends Error {}
+class TemplateMissingMetadataAttribute extends Error {
+    public attribute: string;
+    constructor(attribute: string) {
+        super(`Missing metadata attribute "${attribute}".`);
+        this.attribute = attribute;
+    }
+
 }
 
-interface NodeRequest {
-    metadata: NodeRequestMetadata;
-    data: Map<string, string>;
+// This error represents a programming error, not a user validation problem.
+class MissingMetadataAttributeError extends Error {
+    public attribute: string;
+    constructor(attribute: string) {
+        super(`Missing metadata attribute "${attribute}".`);
+        this.attribute = attribute;
+    }
+}
+class MissingMetadataError extends Error {}
+
+/**
+ * This class contains required metadata for NodeTemplate objects.
+ * (Node objects can contain extra metadata, this defines a subset.)
+ */
+class NodeTemplateMetadata {
+    public type: string;
+    public parent: string;
+
+    /**
+     * Attempt to interpret the given object as a NodeTemplateMetadata object.
+     *
+     * If the object is missing required attributes a TemplateMissingMetadataAttribute
+     * Error will be thrown.
+     */
+    public static checkedInterpretObject(o : object) {
+        for (const a of ['type', 'parent']) {
+            if (!(a in o)) {
+                throw new TemplateMissingMetadataAttribute(`${a}`);
+            }
+        }
+        if ('id' in o) {
+            throw new TemplateContainsId();
+        }
+        return o as NodeMetadata;
+    }
+
+    private constructor() {}
+}
+
+/**
+ * NodeTemplate objects are prototype - think incomplete - versions of Node
+ * objects. They are submitted by users as requests to create full fledged
+ * Nodes.
+ */
+class NodeTemplate {
+    metadata: NodeTemplateMetadata;
+    data?: object;
+
+    public static checkedInterpretObject(o : object) {
+        if (!('metadata' in o)) {
+            throw new TemplateMissingMetadata();
+        }
+
+        // Ignore return value, just forward exception
+        NodeTemplateMetadata.checkedInterpretObject(o['metadata']);
+        return o as NodeTemplate;
+    }
+}
+
+/**
+ * This class contains required metadata for Node objects.
+ * (Node objects can contain extra metadata, this defines a subset.)
+ */
+class NodeMetadata {
+    public id : string;
+    public type: string;
+    public parent: string;
+
+    public static fromObject(o : object) {
+        let us = NodeMetadata.fromArgs(o['id'], o['type'], o['parent']);
+        Object.assign(o, us);
+        return us;
+    }
+    public static fromArgs(id :string, type : string, parent: string) {
+        let us = new this();
+        us.id = id;
+        us.parent = parent;
+        us.type = type;
+        return us;
+    }
+
+    public static checkedInterpretObject(o : object) {
+        for (const a of ['id', 'type', 'parent']) {
+            if (!(a in o)) {
+                throw new MissingMetadataAttributeError(`${a}`);
+            }
+        }
+        return o as NodeMetadata;
+    }
+
+    public static interpretObject(o : object) {
+        //ifdef DEBUG
+        return NodeMetadata.checkedInterpretObject(o);
+        //endif DEBUG
+        //return o as NodeMetadata;
+    }
+
+    private constructor() {}
+}
+
+class Node {
+    metadata: NodeMetadata;
+    data?: object;
+
+    public static checkedInterpretObject(o : object) {
+        if (!('metadata' in o)) {
+            throw new MissingMetadataError();
+        }
+
+        // Ignore return value, just forward exception
+        NodeMetadata.checkedInterpretObject(o['metadata']);
+        return o as Node;
+    }
+
+    public static interpretObject(o : object) {
+        // ifdef DEBUG
+        return Node.checkedInterpretObject(o);
+        // endif DEBUG
+        //return o as Node;
+    }
 }
 
 class NodeCounter {
@@ -43,6 +167,9 @@ class NodesController {
     public initializeRoutes() {
         this.handleGetNodes = this.handleGetNodes.bind(this);
         this.router.get(this.path, this.handleGetNodes);
+
+        this.handlePostNodes = this.handlePostNodes.bind(this);
+        this.router.post(this.path, this.handlePostNodes);
     }
 
     other = (request: express.Request, response: express.Response) => {
@@ -64,6 +191,21 @@ class NodesController {
         }
     }
 
+    /**
+     *
+     * /nodes - GET - Get high level information about many nodes at once
+     *
+     * Parameters:
+     * - (o) type - Type of nodes ids to return
+     * - (o) state - Status of node ids to return, if not supplied nodes of all
+     *   state types will be returned
+     * - (o) metadata - Comma separated list of metadata to provide with the nodes,
+     *   (in addition to ID) if not supplied, all metadata will be returned
+     * - (o) new=<id> - Return only nodes newer than the given id, if none are newer
+     *   the request will wait until the client times out or one or more nodes are
+     *   added
+     *
+     */
     private handleGetNodes(request: express.Request, response: express.Response) {
         const supported_args = new Set(["type", "state", "metadata", "new"]);
 
@@ -79,7 +221,7 @@ class NodesController {
         let nodes : Node[] = [];
         for (const n of this.nodes) {
             nodes.push({
-                "metadata": new Object(n.metadata),
+                "metadata": NodeMetadata.fromObject(n.metadata),
             })
         }
 
@@ -125,24 +267,61 @@ class NodesController {
                         new_map[k] = v;
                     }
                 }
-                n.metadata = new_map;
+                n.metadata = NodeMetadata.interpretObject(new_map);
             }
         }
 
+        // TODO Pagination
         response.send(nodes);
     }
     
-    // TODO Type for node request
-    private addNewNode(node_request: any) {
+    private addTemplatedNode(node_request: NodeTemplate) {
+        let new_id = this.idGenerator.next();
+        node_request['metadata']['id'] = new_id;
+        this.nodes.push(Node.interpretObject(node_request));
+
+        console.log("Adding new node.")
     }
 
-    private handlePutNodes(request: express.Request, response: express.Response) {
-        // TODO Actually convert these into new bodies
-        //
-        const node_requests: Node[] = request.body;
-        for (const n of node_requests) {
-            //
+    /**
+     *
+     *
+     * /nodes - POST - Add a new node with given metadata & data
+     *
+     * Node should be formatted as follows (multiple can be submitted at the same time)::
+     *     [
+     *         {
+     *             metadata: {
+     *                 type: "<any-supported-type>",
+     *                 parent: "<root | an existing node ID>"
+     *                 // An ID will be automatically generated by the server
+     *                 // The task will automatically be place in the incipient state
+     *             }
+     *             data: {
+     *             }
+     *         } //, Optional additional nodes
+     *     ]
+     * */
+    private handlePostNodes(request: express.Request, response: express.Response) {
+        console.log("Adding new node.")
+        if (!(request.body instanceof Array)) {
+            response.status(httpStatus.BAD_REQUEST).send("POST data did not contain Array as top-level");
+            return;
         }
+        for (const n of request.body) {
+            try {
+                 // Ignore return val, just check exception
+                NodeTemplate.checkedInterpretObject(n);
+            } catch (e) {
+                response.status(httpStatus.BAD_REQUEST).send(e.message);
+                return;
+            }
+        }
+        let node_requests: NodeTemplate[] = request.body;
+        for (const n of node_requests) {
+            this.addTemplatedNode(n);
+        }
+        response.status(httpStatus.OK).send();
     }
 }
 
